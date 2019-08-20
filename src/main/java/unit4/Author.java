19bd543bf18d4e447a7c837ca1a887048eb26fb7
Alexey Black <anra582@gmail.com>
2019-08-20 13:39:25 +0700

    Test commit. Added Author class.

---
+++ src/main/java/unit4/Author.java
@@ -0,0 +1,11 @@
+package unit4;
+
+public class Author {
+    private String name;
+    private String birthPlace;
+
+    public Author(String name, String birthPlace) {
+        this.name = name;
+        this.birthPlace = birthPlace;
+    }
+}
